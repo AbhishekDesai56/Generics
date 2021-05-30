354b741bc6472b2a516754d31358b12a8923cc9f
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-05-31 01:43:32 +0530

    Maximum Float method is made along with test case

--- src/main/java/com/generic/GetMaximumTest.java
+++ src/main/java/com/generic/GetMaximumTest.java
@@ -12,4 +12,15 @@ public class GetMaximumTest {
         }
         return max;
     }
+
+    public static Float testFloatMaximum(Float x, Float y, Float z) {
+        Float max = x;
+        if (y.compareTo(max) > 0) {
+            max = y;
+        }
+        if (z.compareTo(max) > 0) {
+            max = z;
+        }
+        return max;
+    }
 }
--- src/test/java/com/generic/MaximumTest.java
+++ src/test/java/com/generic/MaximumTest.java
@@ -7,6 +7,9 @@ public class MaximumTest {
     Integer[] maxAtFirstPosition = {3,2,1};
     Integer[] maxAtSecondPosition = {2,3,1};
     Integer[] maxAtThirdPosition = {2,1,3};
+    Float[] maxFloatAtFirstPosition = {3.0f,2.0f,1.0f};
+    Float[] maxFloatAtSecondPosition = {2.0f,3.0f,1.0f};
+    Float[] maxFloatAtThirdPosition = {2.0f,1.0f,3.0f};
 
     @Test
     public void integerMaxAtFirstPosition() {
@@ -24,5 +27,24 @@ public class MaximumTest {
     public void integerMaxAtThirdPosition() {
         Integer maxZ =  GetMaximumTest.testIntegerMaximum(maxAtThirdPosition[0], maxAtThirdPosition[1], maxAtThirdPosition[2]);
         Assertions.assertEquals(maxAtThirdPosition[2], maxZ);
+    }
+
+
+    @Test
+    public void floatMaxAtFirstPosition() {
+        Float maxX =  GetMaximumTest.testFloatMaximum(maxFloatAtFirstPosition[0], maxFloatAtFirstPosition[1], maxFloatAtFirstPosition[2]);
+        Assertions.assertEquals(maxFloatAtFirstPosition[0], maxX);
+    }
+
+    @Test
+    public void floatMaxAtSecondPosition() {
+        Float maxY =  GetMaximumTest.testFloatMaximum(maxFloatAtSecondPosition[0], maxFloatAtSecondPosition[1], maxFloatAtSecondPosition[2]);
+        Assertions.assertEquals(maxFloatAtSecondPosition[1], maxY);
+    }
+
+    @Test
+    public void floatMaxAtThirdPosition() {
+        Float maxZ =  GetMaximumTest.testFloatMaximum(maxFloatAtThirdPosition[0], maxFloatAtThirdPosition[1], maxFloatAtThirdPosition[2]);
+        Assertions.assertEquals(maxFloatAtThirdPosition[2], maxZ);
     }
 }
