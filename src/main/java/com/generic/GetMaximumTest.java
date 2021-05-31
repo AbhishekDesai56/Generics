5026ab7d9774d97dc379388e6f31ce0a9c0f514f
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-05-31 07:53:23 +0530

    Added Print method

--- src/main/java/com/generic/GetMaximumTest.java
+++ src/main/java/com/generic/GetMaximumTest.java
@@ -1,7 +1,7 @@
 package com.generic;
 
 
-public class GetMaximumTest <E extends Comparable<E>> {
+public class GetMaximumTest<E extends Comparable<E>> {
     public E x;
     public E y;
     public E z;
@@ -24,9 +24,14 @@ public class GetMaximumTest <E extends Comparable<E>> {
         if (z.compareTo(max) > 0) {
             max = z;
         }
-        if(parameterAdd.compareTo(max) > 0) {
+        if (parameterAdd.compareTo(max) > 0) {
             max = parameterAdd;
         }
+        print(x, y, z, parameterAdd, max);
         return (E) max;
+    }
+
+    private void print(E x, E y, E z, E parameterAdd, E max) {
+        System.out.println("X: " + x + " ,Y: " + y + " ,Z: " + z + " and Fourth Parameter:" + parameterAdd + " max: " + max);
     }
 }
